01cea0cba0310de6a330785d553f48d07c874d23
Aleksey Ponomarev <dazed39@mail.ru>
2021-04-25 13:17:13 +0200

    add contacts links into footer, other little fixes

--- js/main.js
+++ js/main.js
@@ -16,6 +16,26 @@
     const $productsDescrContainer = document.querySelector('.production__description');
     const $productsDescrInner = document.querySelector('.production__description-inner');
 
+    // Плавная прокрутка страницы:
+
+    function smoothScroll(element) {
+      window.scroll({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+      });
+    };
+
+    const scrollTo = document.querySelectorAll('[data-scroll]');
+
+    [...scrollTo].forEach(function (elemScroll) {
+      elemScroll.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = elemScroll.getAttribute('data-scroll').replace('#', '');
+        smoothScroll(document.getElementById(id));
+      });
+    });
+
     window.addEventListener('scroll', () => {
       pageAnimation();
     });
