1e30efb13cab503dab8aa6db00f35b560fbc2923
Aleksey Ponomarev <dazed39@mail.ru>
2021-04-27 21:33:33 +0200

    fix form, add advantages section

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@
     const $body = document.querySelector('.body');
     const $heroSection = document.querySelector('.hero');
     const $aboutSection = document.querySelector('.about');
+    const $advantagesSection = document.querySelector('.advantages');
     const $productionSection = document.querySelector('.production');
     const $partnersSection = document.querySelector('.partners');
     const $overlay = document.querySelector('.overlay');
@@ -15,6 +16,10 @@
     const $productsButtons = document.querySelectorAll('.production__item-btn');
     const $productsDescrContainer = document.querySelector('.production__description');
     const $productsDescrInner = document.querySelector('.production__description-inner');
+
+    // Прописываем текущий год в блок Copyright:
+
+    document.querySelector('.copyright__year').innerText = new Date().getFullYear();
 
     // Плавная прокрутка страницы:
 
@@ -62,10 +67,10 @@
     // GSAP Animations:
 
     const headerAnimation = gsap.timeline({
-      paused: true,
-      reversed: true,
-    })
-    .from('.header__logo-icon', {
+        paused: true,
+        reversed: true,
+      })
+      .from('.header__logo-icon', {
         duration: 1,
         y: 15,
         scale: .75,
@@ -83,11 +88,13 @@
         stagger: .35,
       });
 
+    headerAnimation.play();
+
     const heroAnimation = gsap.timeline({
-      paused: true,
-      reversed: true,
-    })
-    .from('.hero__title', {
+        paused: true,
+        reversed: true,
+      })
+      .from('.hero__title', {
         duration: 1,
         scale: .75,
         opacity: 0,
@@ -98,11 +105,13 @@
         opacity: 0,
       });
 
+    heroAnimation.play();
+
     const aboutAnimation = gsap.timeline({
-      paused: true,
-      reversed: true,
-    })
-    .from('.about__title', {
+        paused: true,
+        reversed: true,
+      })
+      .from('.about__title', {
         duration: .75,
         scale: .75,
         opacity: 0,
@@ -114,33 +123,44 @@
         stagger: .25,
       });
 
+    const advantagesAnimation = gsap.timeline({
+        paused: true,
+        reversed: true,
+      })
+      .from('.advantages__item', {
+        duration: 1,
+        y: -35,
+        opacity: 0,
+        stagger: .25,
+      });
+
     const productionAnimations = gsap.timeline({
-      paused: true,
-      reversed: true,
-    })
-    .from('.production__title', {
-      duration: 1,
-      y: -50,
-      opacity: 0,
-    });
+        paused: true,
+        reversed: true,
+      })
+      .from('.production__title', {
+        duration: 1,
+        y: -50,
+        opacity: 0,
+      });
 
     const partnersAnimations = gsap.timeline({
-      paused: true,
-      reversed: true,
-    })
-    .from('.partners__item', {
-      duration: .75,
-      y: "random(-35, 35)",
-      opacity: 0,
-      stagger: .25,
-    });
-
-    headerAnimation.play();
-    heroAnimation.play();
+        paused: true,
+        reversed: true,
+      })
+      .from('.partners__item', {
+        duration: .75,
+        y: "random(-35, 35)",
+        opacity: 0,
+        stagger: .25,
+      });
 
     const pageAnimation = () => {
       if (document.documentElement.clientHeight / 2 >= $aboutSection.getBoundingClientRect().top) {
         aboutAnimation.play();
+      };
+      if (document.documentElement.clientHeight / 2 >= $advantagesSection.getBoundingClientRect().top) {
+        advantagesAnimation.play();
       };
       if (document.documentElement.clientHeight / 2 >= $productionSection.getBoundingClientRect().top) {
         productionAnimations.play();
@@ -150,6 +170,21 @@
         stopAutoListProducts();
       };
     };
+
+    const openPopup = gsap.timeline({
+        paused: true,
+        reversed: true,
+      })
+      .from('.popup', {
+        duration: 1.2,
+        opacity: 0,
+        ease: "expo.out",
+        y: -150,
+      })
+      .from('.popup', {
+        duration: 0.5,
+        scale: 0.85,
+      }, "-=1");
 
     // GSAP Active Products Animations:
 
@@ -240,6 +275,133 @@
     }));
 
     pageAnimation();
+
+    // Открытие и закрытие Popup'а, сообщающего результат отправки сообщения:
+
+    const $modals = document.querySelectorAll('.popups');
+    const $modalOverlay = document.querySelector('.popups__overlay');
+    const $popupEmail = document.querySelector('.popup-email');
+    const $popupCloseBtn = document.querySelectorAll('.popup__close');
+
+    const popupOpen = (popup, icon, message) => {
+      disableScroll();
+      closeAllPopups();
+      $body.classList.add('body--lock');
+      popup.parentNode.classList.add('popups__overlay--visible');
+      popup.classList.add('popup--active');
+      openPopup.play();
+      if (popup.classList.contains('popup-email')) {
+        popup.querySelector('.popup-email__img').setAttribute('src', icon);
+        popup.querySelector('.popup-email__message').innerText = message;
+      };
+    };
+
+    const closeAllPopups = () => {
+      $modals.forEach((el) => {
+        el.querySelector('.popup').classList.remove('popup--active');
+      });
+    };
+
+    $modalOverlay.addEventListener('click', (e) => {
+      if (e.target == $modalOverlay) {
+        popupClose($modalOverlay);
+      };
+    });
+
+    const popupClose = (popup) => {
+      enableScroll();
+      $body.classList.remove('body--lock');
+      openPopup.reverse();
+      popup.classList.remove('popups__overlay--visible');
+      closeAllPopups();
+    };
+
+    $popupCloseBtn.forEach((el) => {
+      el.addEventListener('click', function (e) {
+        popupClose(el.closest('.popups__overlay'));
+        e.preventDefault();
+      });
+    });
+
+    document.addEventListener('keydown', function (e) {
+      if (e.keyCode === 27) {
+        const $popupActive = document.querySelector('.popups__overlay--visible');
+        popupClose($popupActive);
+      };
+    });
+
+    // Обработка формы:
+
+    const $formBody = document.querySelector('.form')
+    const $form = document.getElementById('feedback');
+    $form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+      e.preventDefault();
+      let error = formValidate($form);
+
+      let formData = new FormData($form);
+
+      if (error === 0) {
+        $formBody.classList.add('form--sending');
+        let response = await fetch('sendmail.php', {
+          method: 'POST',
+          body: formData
+        });
+        if (response.ok) {
+          let result = await response.json();
+          // alert(result.message);
+          popupOpen($popupEmail, './img/svg/email-ok.svg', result.message);
+          $form.reset();
+          $formBody.classList.remove('form--sending');
+        } else {
+          popupOpen($popupEmail, './img/svg/email-error.svg', 'Возникла ошибка при отправке формы');
+          $formBody.classList.remove('form--sending');
+        }
+      } else {
+        popupOpen($popupEmail, './img/svg/email-error.svg', 'Заполните обязательные поля');
+      };
+
+    };
+
+    function formValidate(form) {
+      let error = 0;
+      let formReq = document.querySelectorAll('.required');
+
+      for (let i = 0; i < formReq.length; i++) {
+        const input = formReq[i];
+        formRemoveError(input);
+
+        if (input.classList.contains('feedback__input--email')) {
+          if (emailTest(input)) {
+            formAddError(input);
+            error++;
+          };
+        } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+          formAddError(input);
+          error++;
+        } else {
+          if (input.value === '') {
+            formAddError(input);
+            error++;
+          };
+        };
+      };
+      return error;
+    };
+
+    function formAddError(input) {
+      input.classList.add('error');
+    };
+
+    function formRemoveError(input) {
+      input.classList.remove('error');
+    };
+
+    // Функция проверки E-mail:
+    function emailTest(input) {
+      return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    };
 
   });
 
